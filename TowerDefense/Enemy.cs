3b423330957d1e50b6f5376e45397643296f2c54
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-05-27 23:26:49 +0200

    kolejne

---
+++ TowerDefense/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(Enemy))]
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    //private Enemy enemy;
+
+    void Start()
+    {
+        //enemy = GetComponent<Enemy>();
+
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+
+        //enemy.speed = enemy.startSpeed;
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            //EndPath();
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+    /*void EndPath()
+    {
+        PlayerStats.Lives--;
+        WaveSpawner.EnemiesAlive--;
+        Destroy(gameObject);
+    }*/
+
+}
